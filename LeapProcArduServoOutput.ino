b2c65f8f475cfa9a7f5d5fc9d880a2b337e45f85
at0mbxmb <at0mbxmb@gmail.com>
2014-02-04 21:20:27 -0800

    adding source for servo control

---
+++ LeapProcArduServoOutput.ino
@@ -0,0 +1,27 @@
+/*
+ * Leap Motion -> Processing -> Arduino -> Motor
+ *
+ * Michelle Leonhart 2014-02-04
+ * @at0mbmxb www.tinwhiskers.net
+ *
+ */
+ 
+#include <AFMotor.h>
+#include <Servo.h> 
+
+Servo servo1;
+
+int servoVal = 10 ;
+
+void setup() {
+  Serial.begin(9600); 
+  
+  servo1.attach(9);
+}
+
+void loop() {
+  if ( Serial.available()) {
+    int input = Serial.read();
+    servo1.write(input);
+  }
+}
